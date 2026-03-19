6df8481638200b95276a2a8fef550509232365c7
ghxstly21 <migtav28@bergen.org>
2026-03-18 22:51:17 -0400

    Switched to using clap & owo-colors

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,45 @@
 use std::process::exit;
-fn main() {
-    let mut args = std::env::args().skip(1);
-    let mut hidden: Vec<String>;
-    let color: String;
-    if args.len() > 1 {
-        loop {
-            let mut curr = args.next().unwrap().as_str();
-            match curr {
-                "-help" => help(),
-                "-hide" => {
-                    while let Some(arg) = args.next() {
-                        hidden.push(arg);
-                    }
-                    if hidden.is_empty() {
-                        eprintln!("feofetch: expected settings to hide after -hide");
-                        exit(1)
-                    }
-                }
-                "-color" => {
-                    match args.next() {
-                        Some(arg) => color = arg,
-                        None => {
-                            eprintln!("feofetch: expected roygbiv color argument for -color");
-                            exit(1)
-                        }
-                    }
-                }
-                _ => panic!("feofetch: unknown argument: {}", curr)
-            }
+use clap::{Parser, ValueEnum};
+use owo_colors::OwoColorize;
+use owo_colors::colors::*;
 
-        }
-
-        while i < end {
-            match args.nth(i).as_str() {
-                "-help" => help(),
-                "-hide" => {
-
-                }
-                _ => panic!("unknown argument: {}", args[i])
-            }
-        }
-        for arg in args.skip(1) {
-
-        }
-    }
-    let supported_platforms = ["macos", "windows", "linux"];
+#[derive(Clone, Debug, ValueEnum)]
+enum Color {
+    // normal colors
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+    // bright colors
+    BrightBlack,
+    BrightRed,
+    BrightGreen,
+    BrightYellow,
+    BrightBlue,
+    BrightMagenta,
+    BrightCyan,
+    BrightWhite,
+    // chooses the color based on current config
+    Auto,
 }
 
-fn help() {
-    println!("feofetch: showing your system information... but made in rust");
-    println!("USAGE: feofetch [OPTIONS]");
-    println!("When run with no arguments, prints all information and uses default coloring");
-    println!("settings\topens a menu to easily edit your current feofetch config");
-    println!("-hide [SETTINGS]\tHides certain system info from output temporarily");
-    println!("-color [COLOR]\tchanges the color of ascii art; ROYGBIV is allowed");
-    println!("-help or --help prints this menu.")
+/// Shows system information
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = "Showing your system information... but made in Rust")]
+struct Args {
+    /// The color to use for ascii art
+    #[arg(short, long, default_value = "auto")]
+    color: Color,
+
+    /// List of information to hide
+    #[arg(long)]
+    hide: Vec<String>,
+}
+fn main() {
+    let args = Args::parse();
+    let supported_platforms = ["macos", "windows", "linux"];
 }
